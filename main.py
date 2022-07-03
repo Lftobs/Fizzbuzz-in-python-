a06964215e930a3dcd5902d70197980d66da612a
Lftobs <106287048+Lftobs@users.noreply.github.com>
2022-07-03 03:54:30 +0100

    Fizzbuzz-using-while-loop

---
+++ main.py
@@ -0,0 +1,14 @@
+
+#fizzbuzz using while loop 
+
+i = 0
+while i < 100:
+     i += 1
+     if i % 3 == 0 and i % 5 == 0:
+     	print('fizzbuzz')
+     elif i % 5 == 0:
+     	print('buzz')
+     elif i % 3 == 0:
+     	print('fizz')
+     else:
+     	print(i)
